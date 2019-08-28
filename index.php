9a48b788390b911817f0f622a507314bbefe688c
ravenusmc <mcuddy77@gmail.com>
2019-08-28 12:55:25 -0400

    finished tsecond section on home page

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <section>
 
   <div class='words_div'>
-    <p class='center section_title'>Write Your ideas; express yourself</p>
+    <p class='center section_title'>Write Your ideas, express yourself</p>
     <p class='margin_fix center section_paragraph'>Let others know what you think and why you think it</p>
     <div class='button_div'>
       <button>How it Works</button>
@@ -18,12 +18,17 @@
   <div class='section_one_image'>
   </div>
 
-  <div>
-    <h1>One</h1>
+  <div class='section_two_image'>
   </div>
 
-  <div>
-    <h1>One</h1>
+  <div class='words_div'>
+    <p class='center section_title'>Get Your ideas ranked</p>
+    <p class='margin_fix center section_paragraph'>In the market place of ideas see what people think of
+      your ideas.
+    </p>
+    <div class='button_div'>
+      <button>How it Works</button>
+    </div>
   </div>
 
   <div>
