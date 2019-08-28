79bb24907460d95d90895cf3ae60d8348adfe115
ravenusmc <mcuddy77@gmail.com>
2019-08-28 12:40:26 -0400

    finished section one on the landing page

--- index.php
+++ index.php
@@ -1,39 +1,37 @@
 <?php include 'view/header.php'; ?>
+<link rel="stylesheet" type="text/css" href="./assets/css/generic.css">
 <link rel="stylesheet" type="text/css" href="./assets/css/landing.css">
 
 <main>
 </main>
 
 <section>
-  <h1>One</h1>
+
+  <div class='words_div'>
+    <p class='center section_title'>Write Your ideas; express yourself</p>
+    <p class='margin_fix center section_paragraph'>Let others know what you think and why you think it</p>
+    <div class='button_div'>
+      <button>How it Works</button>
+    </div>
+  </div>
+
+  <div class='section_one_image'>
+  </div>
+
+  <div>
+    <h1>One</h1>
+  </div>
+
+  <div>
+    <h1>One</h1>
+  </div>
+
+  <div>
+    <h1>One</h1>
+  </div>
+
+  <div>
+    <h1>One</h1>
+  </div>
+
 </section>
-
-<section>
-  <h1>TWO</h1>
-</section>
-
-
-
-<!-- <div class="container height_fix">
-  <div class="row margin_fix"> -->
-    <!-- Start of login form -->
-    <!-- <form method="post" class="col s12">
-      <div class="row">
-        <div class="input-field col s12">
-          <input name='username' id="username" type="text" class="validate">
-          <label for="username">Username</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s12">
-          <input name='password' id="password" type="password" class="validate">
-          <label for="password">Password</label>
-        </div>
-      </div>
-      <button type="submit" name="login" class="btn btn-primary form-submit-btn">Login</button>
-      <a class="waves-effect waves-light btn" href="signup.php">Sign Up</a>
-    </form> -->
-    <!-- End of login form -->
-
-  <!-- </div>
-</div> -->
