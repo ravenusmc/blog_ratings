880ed2c25666d72c73b9451e881056239def418f
ravenusmc <mcuddy77@gmail.com>
2019-08-29 08:42:58 -0400

    finished title area on landing page

--- index.php
+++ index.php
@@ -3,6 +3,12 @@
 <link rel="stylesheet" type="text/css" href="./assets/css/landing.css">
 
 <main>
+
+  <div class='title_area'>
+    <h1 class='title font'>Blog Ratings</h1>
+    <h3 class='sub_title font'>Write, Share, Get Ranked</h3>
+  </div>
+
 </main>
 
 <section>
