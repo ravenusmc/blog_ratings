93b930efef149b6530cfe2b64eb31eac789e7abb
ravenusmc <mcuddy77@gmail.com>
2019-08-30 11:49:18 -0400

    finished middle section on background page

--- background.php
+++ background.php
@@ -9,6 +9,47 @@
 
   <h1 class='title'>How it Works</h1>
 
+  <section>
+
+    <div class='section_one_image_background'>
+    </div>
+
+    <div>
+      <div class='paragraph_div'>
+        <h1 class='font center'>Join</h1>
+        <p class='font center'>First it's free to join. Create a profile with some basic information
+          about yourself. Once you've joined you can start writing.
+        </p>
+      </div>
+    </div>
+
+    <div>
+      <div class='paragraph_div'>
+        <h1 class='font center'>Write</h1>
+        <p class='font center'>Second write! Write about anything that your heart
+          desires. Once you write though you get ranked!
+        </p>
+      </div>
+    </div>
+
+    <div class='section_two_image_background'>
+    </div>
+
+    <div class='section_three_image_background'>
+    </div>
+
+    <div>
+      <div class='paragraph_div'>
+        <h1 class='font center'>Rated</h1>
+        <p class='font center'>Third get rated. The most popular posting move to the
+          top. But beware there's no commenting here! 
+        </p>
+      </div>
+    </div>
+
+
+  </section>
+
 </main>
 
 <?php include 'view/Footer.php'; ?>
