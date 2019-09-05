90d0d326af11f30bda02c4daf6a982aa11835ab7
ravenusmc <mcuddy77@gmail.com>
2019-09-05 16:13:47 -0400

    added more to signup page

--- signup.php
+++ signup.php
@@ -1,5 +1,22 @@
 <?php include 'view/header.php'; ?>
+<link rel="stylesheet" type="text/css" href="./assets/css/generic.css">
+<link rel="stylesheet" type="text/css" href="./assets/css/login.css">
 
-sign up
+<main>
+
+  <form class='form'>
+
+    <div class='form-item'>
+      <input type='text' class='form-input' placeholder="Username" aria-label="Username">
+    </div>
+
+    <div class='form-item'>
+      <input type='password' class='form-input' placeholder="Password" aria-label="Password">
+    </div>
+
+    <button class='form-button' type='submit'>Submit</button>
+  </form>
+
+</main>
 
 <?php include 'view/Footer.php'; ?>
