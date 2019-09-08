5039fc6f4814332216b7c80b0930f4e80b7d6766
ravenusmc <mcuddy77@gmail.com>
2019-09-08 11:44:58 -0400

    improving sign up form

--- signup.php
+++ signup.php
@@ -1,44 +1,61 @@
+<?php
+  // Start a session
+  if (!isset($_SESSION)) {
+    session_start();
+  }
+  //Pulling in the databases
+  //require('./model/database.php');
+  //global $db;
+
+  if (isset($_POST["login"])){
+    $username = filter_input(INPUT_POST, 'username');
+  }
+
+?>
+
 <?php include 'view/header.php'; ?>
 <link rel="stylesheet" type="text/css" href="./assets/css/generic.css">
-<link rel="stylesheet" type="text/css" href="./assets/css/login.css">
+<link rel="stylesheet" type="text/css" href="./assets/css/signup.css">
 
 <main>
 
-  <form class='form'>
+  <h1 class>
+
+  <form method="post" class='form'>
 
     <div class='form-item'>
-      <input type='text' class='form-input' placeholder="Username" aria-label="Username">
+      <input type='text' class='form-input' name='username' placeholder="User Name" aria-label="Username">
     </div>
 
     <div class='form-item'>
-      <input type='text' class='form-input' placeholder="Firstname" aria-label="Firstname">
+      <input type='text' class='form-input' name='firstname' placeholder="First Name" aria-label="Firstname">
     </div>
 
     <div class='form-item'>
-      <input type='text' class='form-input' placeholder="Lastname" aria-label="Lastname">
+      <input type='text' class='form-input' name='lastname' placeholder="Last Name" aria-label="Lastname">
     </div>
 
     <div class='form-item'>
-      <input type='text' class='form-input' placeholder="Address" aria-label="Address">
+      <input type='text' class='form-input' name='address' placeholder="Address" aria-label="Address">
     </div>
 
     <div class='form-item'>
-      <input type='text' class='form-input' placeholder="City" aria-label="City">
+      <input type='text' class='form-input' name='city' placeholder="City" aria-label="City">
     </div>
 
     <div class='form-item'>
-      <input type='text' class='form-input' placeholder="State" aria-label="State">
+      <input type='text' class='form-input' name='state' placeholder="State" aria-label="State">
     </div>
 
     <div class='form-item'>
-      <input type='password' class='form-input' placeholder="Password" aria-label="Password">
+      <input type='password' class='form-input' name='password_1' placeholder="Password" aria-label="Password">
     </div>
 
     <div class='form-item'>
-      <input type='password' class='form-input' placeholder="Password" aria-label="Password">
+      <input type='password' class='form-input' name='password_2' placeholder="Password" aria-label="Password">
     </div>
 
-    <button class='form-button' type='submit'>Submit</button>
+    <button type="submit" name="login" class='form-button' type='submit'>Submit</button>
   </form>
 
 </main>
