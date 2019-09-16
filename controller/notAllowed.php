e53cb34c81d3ecaf1b414007fb7abff13cc5d118
ravenusmc <mcuddy77@gmail.com>
2019-09-16 16:13:14 -0400

    fixed log out page

--- controller/notAllowed.php
+++ controller/notAllowed.php
@@ -1,2 +1,9 @@
-<h1>GO AWAY</h1>
-<a class="waves-effect waves-light btn" href="../index.php">Login</a>
+<!-- Bringing in CSS -->
+<link rel="stylesheet" type="text/css" href="../assets/css/generic.css">
+<!-- CSS for this page -->
+<link rel="stylesheet" type="text/css" href="../assets/css/notAllowed.css">
+
+<div id='notAllowedDiv'>
+  <h1 class='font center color'>GO AWAY</h1>
+  <a class="color" href="../index.php">Login</a>
+</div>
